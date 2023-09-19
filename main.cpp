c3b0999bf53b9234edc82eb0c9c1263fcd2286e3
shuaifanfan <1596447104@qq.com>
2023-09-20 00:13:06 +0800

    perfect

--- main.cpp
+++ main.cpp
@@ -12,9 +12,12 @@ using namespace glm;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+
+const unsigned int SCR_WIDTH = 800*2.35;
+const unsigned int SCR_HEIGHT = 600*1.5;
 
 vec3 cameraPos = vec3(0.0f, 0.0f, 3.0f);
 vec3 cameraFront = vec3(0.0f, 0.0f, -1.0f);
@@ -22,6 +25,13 @@ vec3 cameraUp = vec3(0.0f, 1.0f, 0.0f);
 
 float deltaTime = 0.0f; // 当前帧与上一帧的时间差
 float lastFrame = glfwGetTime(); // 上一帧的时间
+
+bool firstMouse = true;
+float yaw = -90.0f;	// 
+float lastX = 800.0f / 2.0;
+float lastY = 600.0 / 2.0;
+float fov = 45.0f;
+float pitch = 0.0f;
 
 int main()
 {
@@ -39,7 +49,9 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -216,9 +228,8 @@ int main()
 		//mat4 view = mat4(1.0f);
 		mat4 projection = mat4(1.0f);
 		//view  = translate(view,vec3(0.0f, 0.0f, -3.0f));
-		projection = perspective(radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f,100.0f);
-		//unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
-		//glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+		
+		projection = perspective(radians(fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f,100.0f);
 		ourShader.setMat4("projection", value_ptr(projection));
 
 		mat4 view = mat4(1.0f); // make sure to initialize matrix to identity matrix first
@@ -231,7 +242,7 @@ int main()
 
 			mat4 model = mat4(1.0f);
 			model = translate(model, cubePositions[i]);
-			model = rotate(model, (float)glfwGetTime() * radians(50.0f) , vec3(float(rand()%3), 1.0f, 0.0f)); //random rotation
+			model = rotate(model, (float)glfwGetTime() * radians(50.0f)*5 , vec3(float(rand()%3), 1.0f, 0.0f)); //random rotation
 			unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
 			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, value_ptr(model));
 			glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -274,4 +285,52 @@ void processInput(GLFWwindow* window)
 		cameraPos -= normalize(cross(cameraFront, cameraUp)) * cameraSpeed;
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		cameraPos += normalize(cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+	lastX = xpos;
+	lastY = ypos;
+
+	float sensitivity = 0.05f; // change this value to your liking
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	::yaw += xoffset;
+	::pitch += yoffset;
+
+	// make sure that when pitch is out of bounds, screen doesn't get flipped
+	if (::pitch > 89.0f)
+		::pitch = 89.0f;
+	if (::pitch < -89.0f)
+		::pitch = -89.0f;
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(::yaw)) * cos(glm::radians(::pitch));
+	front.y = sin(glm::radians(::pitch));
+	front.z = sin(glm::radians(::yaw)) * cos(glm::radians(::pitch));
+	cameraFront = glm::normalize(front);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	if (fov >= 1.0f && fov <= 45.0f)
+		fov -= yoffset*3;
+	if (fov <= 1.0f)
+		fov = 1.0f;
+	if (fov >= 45.0f)
+		fov = 45.0f;
 }
